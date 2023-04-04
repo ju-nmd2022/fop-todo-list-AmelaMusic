99a532423f90d07093dd3db2a6756298dc10cdc2
AmelaMusic <muam21gk@student.ju.se>
2023-04-04 16:32:24 +0200

    commit

--- todolist.js
+++ todolist.js
@@ -1,16 +1,23 @@
-// let taskList = [];
-
 function adTaskList() {
   var input = document.getElementById("input").value;
   var taskList = document.createElement("li");
 
   taskList.appendChild(document.createTextNode(input));
 
-  document.getElementById("list").appendChild(taskList);
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
 
-  const button = document.createElement("button");
-  button.innerText = "Delete";
-  document.getElementById("containerList").appendChild(button);
+  const markedButton = document.createElement("button");
+  markedButton.innerText = "Done";
+
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  container.appendChild(taskList);
+  container.appendChild(deleteButton);
+  container.appendChild(markedButton);
+
+  document.getElementById("list").appendChild(container);
 
   //   localStorage.setItem(newListItem);
 }
