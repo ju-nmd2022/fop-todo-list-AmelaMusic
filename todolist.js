a20e3a867029bd0e855fb366301261fe81c99ed9
AmelaMusic <muam21gk@student.ju.se>
2023-04-09 00:56:09 +0200

    commit

--- todolist.js
+++ todolist.js
@@ -54,11 +54,88 @@ function displaySavedTasks() {
       const item = document.createElement("li");
       item.appendChild(document.createTextNode(task));
       // document.getElementById("list").appendChild(item);
-      savedElement.appendChild(item);
+
+      const markedButton = document.createElement("button");
+      markedButton.innerText = "Done";
+      markedButton.onclick = markTask;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.innerText = "Delete";
+      deleteButton.onclick = deleteTask;
+
+      const buttonContainer = document.createElement("div");
+      buttonContainer.classList.add("buttonContainer");
+      buttonContainer.appendChild(markedButton);
+      buttonContainer.appendChild(deleteButton);
+
+      const container = document.createElement("div");
+      container.classList.add("container");
+
+      container.appendChild(item);
+      container.appendChild(buttonContainer);
+
+      savedElement.appendChild(container);
+
       i++;
     }
   }
 }
+
+// function displaySavedTasks() {
+//   const savedElement = document.getElementById("list");
+
+//   let tasksArray = JSON.parse(localStorage.getItem("tasks"));
+
+//   if (tasksArray != null) {
+//     let i = 0;
+//     while (i < tasksArray.length) {
+//       const task = tasksArray[i];
+//       const item = document.createElement("li");
+//       item.appendChild(document.createTextNode(task));
+
+//       const markedButton = document.createElement("button");
+//       markedButton.innerText = "Done";
+//       markedButton.onclick = markTask;
+
+//       const deleteButton = document.createElement("button");
+//       deleteButton.innerText = "Delete";
+//       deleteButton.onclick = deleteTask;
+
+//       const buttonContainer = document.createElement("div");
+//       buttonContainer.classList.add("buttonContainer");
+//       buttonContainer.appendChild(markedButton);
+//       buttonContainer.appendChild(deleteButton);
+
+//       const container = document.createElement("div");
+//       container.classList.add("container");
+
+//       container.appendChild(item);
+//       container.appendChild(buttonContainer);
+
+//       savedElement.appendChild(container);
+
+//       i++;
+//     }
+//   }
+// }
+
+// function displaySavedTasks() {
+//   const savedElement = document.getElementById("list");
+
+//   let tasksArray = JSON.parse(localStorage.getItem("tasks"));
+
+//   if (tasksArray != null) {
+//     let i = 0;
+//     while (i < tasksArray.length) {
+//       const task = tasksArray[i];
+//       const item = document.createElement("li");
+//       item.appendChild(document.createTextNode(task));
+//       // document.getElementById("list").appendChild(item);
+//       savedElement.appendChild(item);
+//       i++;
+//     }
+//   }
+// }
 
 window.onload = function () {
   displaySavedTasks();
