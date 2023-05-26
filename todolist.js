f5a5991022eebdcf9b6502c913ad56f64498873e
AmelaMusic <muam21gk@student.ju.se>
2023-05-26 22:33:42 +0200

    references update

--- todolist.js
+++ todolist.js
@@ -1,36 +1,44 @@
+// Source: most of code inspired from lecture fruitshop + linked youtube video
+// source: other lectures (while loop)
+// source: line 60, 62,68,70,71,74,80 based on youtube video: https://www.youtube.com/watch?v=pRkHOD_nkH4 accessed april 2023
+
+// ads new tasks to the list and stores them in local storage
 function adTaskList() {
   var input = document.getElementById("input").value;
   var taskList = document.createElement("li");
-  //set the id for the tasks
+  //set the unique id for the tasks (converting the current time to a string)
   const id = new Date().getTime().toString();
 
   taskList.appendChild(document.createTextNode(input));
   taskList.setAttribute("data-id", id);
 
+  //creates "mark as done" button  
   const markedButton = document.createElement("button");
-
   markedButton.style.backgroundImage = 'url("check-mark.png")';
   markedButton.classList.add("markedButton");
   markedButton.onclick = markTask;
 
+  // creates a "delete" button
   const deleteButton = document.createElement("button");
-
   deleteButton.style.backgroundImage = 'url("trash-can.png")';
   deleteButton.classList.add("deleteButton");
   deleteButton.onclick = deleteTask;
 
+  // creates a container for the buttons and ads the buttons in it
   const buttonContainer = document.createElement("div");
   buttonContainer.classList.add("buttonContainer");
   buttonContainer.appendChild(markedButton);
   buttonContainer.appendChild(deleteButton);
 
+  // creates a container for the task list item,sets the "data-id" attribute.
+  //Ads the button container and task list item
   const container = document.createElement("div");
   container.classList.add("container");
   container.dataset.id = id;
-
   container.appendChild(taskList);
   container.appendChild(buttonContainer);
 
+  //appends the container element to the list with the ID "list"
   document.getElementById("list").appendChild(container);
 
   // store in local storage
@@ -40,16 +48,17 @@ function adTaskList() {
   localStorage.setItem("tasks", JSON.stringify(tasksArray));
 }
 
-// source: lecture fruitshop
+
+
 function deleteTask() {
   const element = this.parentNode.parentNode;
   element.parentNode.removeChild(element);
-
+  //retrieve the ID from the task
   const id = element.dataset.id;
-  // delete task. //Source: remove with task identifier:
-  //https://www.google.com/search?q=delete+from+local+storage+when+deleting+input&hl=sv&sxsrf=APwXEdfHYcTdacitm-3tDxQFEZYeO8wmyw:1681036361434&source=lnms&tbm=vid&sa=X&ved=2ahUKEwjH07W5zJz-AhXyQ_EDHey0BOoQ_AUoAXoECAEQAw&biw=1036&bih=621&dpr=2#fpstate=ive&vld=cid:ebe8b768,vid:pRkHOD_nkH4
+  // delete task from local storage
   var tasksArray = JSON.parse(localStorage.getItem("tasks")) || [];
   const newTaskArray = tasksArray.filter((task) => task.id != id);
+  //stores updated taskarray in local storage
   localStorage.setItem("tasks", JSON.stringify(newTaskArray));
 }
 
@@ -61,6 +70,7 @@ function markTask() {
   let tasksArray = JSON.parse(localStorage.getItem("tasks"));
   const task = tasksArray.find((task) => task.id === id);
   task.done = true;
+   //stores updated taskarray in local storage
   localStorage.setItem("tasks", JSON.stringify(tasksArray));
 }
 
@@ -76,19 +86,20 @@ function displaySavedTasks() {
       const item = document.createElement("li");
       item.appendChild(document.createTextNode(task.task));
       item.dataset.id = task.id;
-
+      
+      //creates "mark as done" button  
       const markedButton = document.createElement("button");
-
       markedButton.style.backgroundImage = 'url("check-mark.png")';
       markedButton.classList.add("markedButton");
       markedButton.onclick = markTask;
 
+       // creates a "delete" button
       const deleteButton = document.createElement("button");
-
       deleteButton.style.backgroundImage = 'url("trash-can.png")';
       deleteButton.classList.add("deleteButton");
       deleteButton.onclick = deleteTask;
 
+      // creates a container for the buttons and ads the buttons in it
       const buttonContainer = document.createElement("div");
       buttonContainer.classList.add("buttonContainer");
       buttonContainer.appendChild(markedButton);
